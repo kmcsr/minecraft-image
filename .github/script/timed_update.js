bdffd85c1fbc9bcc4994ad6a4b6991fcb183ac5f
zyxkad <zyxkad@gmail.com>
2023-03-02 22:20:53 -0700

    update `timed_update.js`

--- .github/script/timed_update.js
+++ .github/script/timed_update.js
@@ -11,20 +11,24 @@ module.exports = async ({github, context}) => {
 	}catch(e){
 		console.error('Unable to read version from cache file:', e);
 	}
+	console.log('Cached version:', version)
 	var res = await github.request('https://api.github.com/repos/Fallen-Breath/MCDReforged/releases/latest');
 	if(res.status != 200){
 		console.error(`Unexpect status ${res.status}`);
 		return;
 	}
 	var ver = res.data.tag_name;
+	console.log('Request version:', ver)
 	if(ver[0] === 'v'){
 		ver = ver.substr(1);
-		if(ver !== version.trim()){
+		if(!version || ver !== version.trim()){
 			try{
 				await fs.writeFile(VERSION_CACHE_PATH, ver + '\n');
+				return true;
 			}catch(e){
 				console.error('Unable to write version to cache file:', e);
 			}
 		}
 	}
+	return false;
 }
